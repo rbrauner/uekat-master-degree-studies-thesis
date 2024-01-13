9737ffa9d7adc04bef944d9636e848e6d18fd1e7
rbrauner <rafalbrauner@gmail.com>
2024-01-13 22:23:04 +0100

    feat(#1): change to class

--- app/index.ts
+++ app/index.ts
@@ -1,31 +1,68 @@
 const Tello = require('tello-drone');
 
-const drone = new Tello();
 
-drone.on("connection", () => {
-    console.log("Connected to drone");
-});
+class App {
+    drone: any;
+    currentCommand: string;
+    commands: string[];
+    locked: boolean;
 
-drone.on("state", (state: number) => {
-    console.log("Recieved State > ", state);
-});
+    constructor() {
+        this.drone = new Tello();
+        this.currentCommand = "";
+        this.commands = [];
+        this.locked = false;
 
-drone.on("send", (err: Error, length: number) => {
-    if (err) console.log(err);
+        this.drone.on("connection", () => {
+            console.log("Connected to drone");
+        });
+        this.drone.on("state", this.onState.bind(this));
+        this.drone.on("send", this.onSend.bind(this));
+        this.drone.on("message", this.onMessage.bind(this));
 
-    console.log(`Sent command is ${length} long`);
-});
-
-drone.on("message", (message: string) => {
-    console.log("Recieved Message > ", message);
-});
-
-drone.on("connection", async () => {
-    try {
-        await drone.send("battery?");
-    } catch (error) {
-        console.log(error)
-        drone.send("land")
-        setTimeout(process.exit)
+        setInterval(this.consumeCommands.bind(this), 1000);
     }
-});
+
+    consumeCommands() {
+        if (this.locked) return;
+
+        this.locked = true;
+        const command = this.commands.shift();
+        if (command) {
+            this.drone.send(command);
+        }
+
+        this.locked = false;
+    }
+
+    onState(state: number) {
+        console.log("Recieved State > ", state);
+    }
+
+    onSend(err: Error, length: number) {
+        if (err) console.log(err);
+
+        console.log(`Sent command is ${length} long`);
+    }
+
+    onMessage(message: string) {
+        console.log("Recieved Message > ", message);
+    }
+
+    async send(command: string) {
+        this.commands.push(command);
+    }
+
+    run() {
+        try {
+            this.send("battery?");
+        } catch (error) {
+            console.log(error)
+            this.drone.send("land")
+            setTimeout(process.exit)
+        }
+    }
+}
+
+const app = new App();
+app.run();

1d343e8e8ff3e07d3b46f04fa9685a5273765b9e
rbrauner <rafalbrauner@gmail.com>
2024-08-12 17:54:18 +0200

    feat(#8): update

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
+from djitellopy import Tello
+import torch
+
 class App:
+    def __init__(self):
+        self.tello = Tello()
+        self.model = None
+
     def run(self):
-        pass
+        try:
+            self.load_model()
+
+            self.tello.connect()
+            self.tello.streamon()
+        except Exception as e:
+            print(e)
+        finally:
+            self.tello.land()
+
+    def load_model(self):
+        self.model = torch.load("tmp/model.pth")
 
 if __name__ == "__main__":
     app = App()
